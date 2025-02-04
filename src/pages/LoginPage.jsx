7bf18385ade0cba2d0ef12772820dcb916ea14be
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-02-04 17:03:54 +0100

    small fixes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const [errorMessage, setErrorMessage] = useState(""); // For displaying login errors
+  const [errorMessage, setErrorMessage] = useState(""); 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -17,17 +17,17 @@ function LoginPage() {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:5005/auth/login", // Backend login route
+        "http://localhost:5005/auth/login", 
         formData
       );
 
       console.log("Login successful", data);
 
-      // Save token in localStorage
+      
       localStorage.setItem("authToken", data.authToken);
 
-      // Redirect to homepage or another page
-      navigate("/"); // You can redirect to any page you want, e.g., homepage or dashboard
+      
+      navigate("/"); 
     } catch (error) {
       console.log("Login error:", error.response?.data || error.message);
       setErrorMessage(error.response?.data?.message || "Login failed. Try again.");
@@ -38,7 +38,7 @@ function LoginPage() {
     <div>
       <h2>Login Page</h2>
       
-      {/* Display error message if any */}
+      
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       
       <form onSubmit={handleSubmit}>
